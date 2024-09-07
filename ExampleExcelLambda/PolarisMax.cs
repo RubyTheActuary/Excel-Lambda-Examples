c5efe268de1ec154d29b76cb71e02664a100ed6d
RubyTheActuary <bryon.robidoux@gmail.com>
2024-09-07 16:55:09 -0500

    This contains Polaris Max for annual ratchet which is examples 1-7 of the prospectus. It also contains Polar Max daily ratchet is 8+ examples in the prospectus. This code exampe only covers the annual piece but could easily be extended for the daily.

---
+++ ExampleExcelLambda/PolarisMax.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examplea
+{
+    
+        internal class PolarisMax
+        {
+            //Straight from Chat GPT Interpretation
+
+
+            // Withdrawal Penalty Calculation
+            public static double WithdrawalPenalty(bool isActivated, double wdTaken, double pCv, double cv, double pMaw)
+            {
+                double noWd = 1;
+                double preActivationWd = 1 - Math.Max(0, wdTaken - pMaw) / (pCv - pMaw);
+                double postActivationWd = 1 - wdTaken / cv;
+                return wdTaken != 0
+                    ? (isActivated ? preActivationWd : postActivationWd)
+                    : noWd;
+            }
+
+            // Income Credit Base Calculation
+            public static double IncomeCreditBase(bool isActivated, bool isAnniversary, bool hasIncomeCredit, double anniversaryCv, double purchasePayments, double wdPenaltyMult, double prevIncomeCreditBase)
+            {
+                if (!isActivated)
+                {
+                    if (hasIncomeCredit)
+                        return prevIncomeCreditBase;
+                    else
+                        return (isAnniversary
+                            ? Math.Max(prevIncomeCreditBase, anniversaryCv + purchasePayments)
+                            : prevIncomeCreditBase + purchasePayments) * wdPenaltyMult;
+                }
+                return 0;
+            }
+
+            // Income Credit Calculation
+            public static double IncomeCredit(double payPeriod, double prevIncomeCreditBase, bool isActivated, bool isAnniversary, double incCredRate)
+            {
+                double picb = payPeriod > 0 ? prevIncomeCreditBase : 0;
+                bool isPreSysWdOnAnniversary = !isActivated && isAnniversary;
+                return isPreSysWdOnAnniversary ? picb * incCredRate : 0;
+            }
+
+            // Anniversary Cv Calculation
+            public static double AnniversaryCv(bool isAnniversary, double payPeriod, double cv)
+            {
+                return !isAnniversary || payPeriod == 0 ? 0 : cv;
+            }
+
+            // Initial Current Income Base Calculation
+            public static double InitialCurrentIncomeBase(bool isAnniversary, double prevIB, double anniversaryCv, double purchasePayments, double wdPenaltyMult)
+            {
+                return (isAnniversary
+                    ? Math.Max(prevIB, anniversaryCv + purchasePayments)
+                    : prevIB + purchasePayments) * wdPenaltyMult;
+            }
+
+            // Income Base Adjustment Calculation
+            public static double IncomeBaseAdjustment(double initialCurrIB, double prevIB, bool isAnniversary, bool isActivated, double incomeCredit)
+            {
+                double delta = initialCurrIB - prevIB;
+                return isAnniversary && !isActivated ? Math.Max(0, incomeCredit - delta) : 0;
+            }
+
+            // Income Base Calculation
+            public static double IncomeBase(double initialCurrIB, double adjustment)
+            {
+                return initialCurrIB + adjustment;
+            }
+
+            // Max Allowable Withdrawal Amount Calculation
+            public static double MaxAllowableWdAmount(double cv, double incomeBase, double mawdRate)
+            {
+                return cv > 0 ? incomeBase * mawdRate : 0;
+            }
+
+            // MAW Stack Calculation
+            public static double[] MawStack(double prevCv, double prevIB, double prevICB, double prevMawa, double payPeriod, double wdTaken, double cv, bool isAnniversary, bool isActivated, double purchasePayments, double mawdRate, double incCredRate)
+            {
+                double anniversaryCv = AnniversaryCv(isAnniversary, payPeriod, cv);
+                double wdPenaltyMult = WithdrawalPenalty(isActivated, wdTaken, prevCv, cv, prevMawa);
+                double incomeCredit = IncomeCredit(payPeriod, prevICB, isActivated, isAnniversary, incCredRate);
+                double initialCurrIB = InitialCurrentIncomeBase(isAnniversary, prevIB, anniversaryCv, purchasePayments, wdPenaltyMult);
+                double adjustment = IncomeBaseAdjustment(initialCurrIB, prevIB, isAnniversary, isActivated, incomeCredit);
+                bool hasIncomeCredit = adjustment > 0;
+                double incomeCreditBase = IncomeCreditBase(isActivated, isAnniversary, hasIncomeCredit, anniversaryCv, purchasePayments, wdPenaltyMult, prevICB);
+                double incomeBase = IncomeBase(initialCurrIB, adjustment);
+                double mawa = MaxAllowableWdAmount(cv, incomeBase, mawdRate);
+
+                return new double[] { cv, incomeBase, incomeCreditBase, mawa, wdPenaltyMult, adjustment, incomeCredit };
+            }
+
+            // Protected Income Payment Calculation
+            public static double ProtectedIncomePayment(double cv, double incomeBase, double pipRate)
+            {
+                return cv == 0 ? pipRate * incomeBase : 0;
+            }
+
+            // Is Anniversary Calculation
+            public static bool IsAnniversary(double payPeriod)
+            {
+                return payPeriod % 1 == 0;
+            }
+
+            // Is Activated Calculation
+            public static bool IsActivated(double payPeriod, double activationPeriod)
+            {
+                return payPeriod >= activationPeriod;
+            }
+        }
+    }
+
+
---
+++ ExampleExcelLambda/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examplea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoPolicyIllustration();
+            Console.WriteLine("Done");
+        }
+
+        private static void DoPolicyIllustration()
+        {
+            #region  Inputs         
+
+            const int periods = 23;
+            const double activationTime = 4.5;
+            const double incomeCreditRate = 0.0525;
+            const double mawRate = 0.0625;
+            const double pipRate = 0.04;
+            var payPeriod = from p in Enumerable.Range(0, periods * 2) select p * 0.5;
+            double[] pp = { 100000.00, 60000.00, 0, 90000.00 };
+            var purchasePayments = Enumerable.Concat(pp, Enumerable.Repeat(0.0, periods - 4));
+            double[] wds = { 5000.00, 0, 19500.00, 0, 24960.00, 0, 24483.00 };
+            var withdrawalsTaken = Enumerable.Concat(Enumerable.Repeat(0.0, 7), Enumerable.Concat(wds, Enumerable.Repeat(0.0, periods - 7 - 5)));
+            double[] accountValue = { 100000.00, 165000.00, 170000.00, 255000.00, 287000.00, 287000.00, 300000.00, 305000.00, 312000.00, 302000.00, 305000.00, 280000.00, 290000.00, 260000.00, 230000.00, 230000.00, 150000.00, 150000.00, 100000.00, 100000.00, 50000.00, 50000.00, 0.00 };
+
+            var inputs = from z in payPeriod.Zip(purchasePayments.Zip(withdrawalsTaken.Zip(accountValue, (first, second) => new Tuple<double, double>(first, second)), (x, y) => new Tuple<double, double, double>(x, y.Item1, y.Item2)), (f, s) => new Tuple<double, double, double, double>(f, s.Item1, s.Item2, s.Item3))
+                         select new { period = z.Item1, purchase = z.Item2, wdTaken = z.Item3, cv = z.Item4 };
+
+            #endregion
+            #region Initialize Program
+            double prevCv = pp[0];
+            double prevIB = pp[0];
+            double prevICB = pp[0];
+            double prevMawa = 0;
+            #endregion
+            #region Write Header
+            ////////////// *********************************** ////////////////////////////////////////////////////
+            const int padWidth = 16;
+            string[] header = { "CV|", "INCOME BASE|", "INC. CREDIT BASE|", "MAWA|", "Wd Penalty|", "Income Base Adj|", "Income Credit|", "PIP" };
+            foreach (var val in header)
+            {
+                Console.Write(val.PadLeft(padWidth));
+            }
+            Console.WriteLine();
+            #endregion
+
+            foreach (var input in inputs)
+            {
+                #region Program By Numbers
+                bool isAnniversary = PolarisMax.IsAnniversary(input.period);
+                bool isAcitivated = PolarisMax.IsActivated(input.period, activationTime);
+
+                double[] result = PolarisMax.MawStack(prevCv, prevIB, prevICB, prevMawa, input.period, input.wdTaken, input.cv, isAnniversary, isAcitivated, input.purchase, mawRate, incomeCreditRate);
+                var protectedIncomePayment = PolarisMax.ProtectedIncomePayment(input.cv, result[1], pipRate);
+
+                prevCv = result[0];
+                prevIB = result[1];
+                prevICB = result[2];
+                prevMawa = result[3];
+                #endregion
+                #region Write Body
+                foreach (double val in result)
+                {
+                    string data = Math.Round(val,2).ToString();
+                    Console.Write(data.PadLeft(padWidth));
+                }
+                Console.Write(Math.Round(protectedIncomePayment,2).ToString().PadLeft(padWidth));
+                Console.WriteLine();
+                #endregion
+            }
+        }
+    }
+}
